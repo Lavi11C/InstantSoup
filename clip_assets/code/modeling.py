3f6cfe83c804f950b04a235fcd02e1719bf80bdd
Lavi11C <127959246+Lavi11C@users.noreply.github.com>
2024-12-18 17:21:00 +0800

    Update modeling.py

--- clip_assets/code/modeling.py
+++ clip_assets/code/modeling.py
@@ -65,32 +65,64 @@ class ClassificationHead(torch.nn.Linear):
         return utils.torch_load(filename)
 
 
+# class ImageClassifier(torch.nn.Module):
+#     def __init__(self, image_encoder, classification_head):
+#         super().__init__()
+#         self.image_encoder = image_encoder
+#         self.classification_head = classification_head
+#         if self.image_encoder is not None:
+#             self.train_preprocess = self.image_encoder.train_preprocess
+#             self.val_preprocess = self.image_encoder.val_preprocess
+
+#     def freeze_head(self):
+#         self.classification_head.weight.requires_grad_(False)
+#         self.classification_head.bias.requires_grad_(False)
+
+#     def forward(self, inputs):
+#         features = self.image_encoder(inputs)
+#         outputs = self.classification_head(features)
+#         return outputs
+
+#     def __call__(self, inputs):
+#         return self.forward(inputs)
+
+#     def save(self, filename):
+#         print(f'Saving image classifier to {filename}')
+#         utils.torch_save(self, filename)
+
+#     @classmethod
+#     def load(cls, filename):
+#         print(f'Loading image classifier from {filename}')
+#         return utils.torch_load(filename)
 class ImageClassifier(torch.nn.Module):
-    def __init__(self, image_encoder, classification_head):
+    def __init__(self, image_encoder, num_classes=1000):
         super().__init__()
         self.image_encoder = image_encoder
-        self.classification_head = classification_head
+        # 替換原有的 classification_head，改用標準的線性分類層
+        self.classifier = torch.nn.Linear(self.image_encoder.output_dim, num_classes)
+        
         if self.image_encoder is not None:
             self.train_preprocess = self.image_encoder.train_preprocess
             self.val_preprocess = self.image_encoder.val_preprocess
 
     def freeze_head(self):
-        self.classification_head.weight.requires_grad_(False)
-        self.classification_head.bias.requires_grad_(False)
+        # 凍結分類層的參數
+        self.classifier.weight.requires_grad_(False)
+        self.classifier.bias.requires_grad_(False)
 
     def forward(self, inputs):
         features = self.image_encoder(inputs)
-        outputs = self.classification_head(features)
+        outputs = self.classifier(features)
         return outputs
 
     def __call__(self, inputs):
         return self.forward(inputs)
 
     def save(self, filename):
-        print(f'Saving image classifier to {filename}')
+        print(f'正在保存圖像分類器到 {filename}')
         utils.torch_save(self, filename)
 
     @classmethod
     def load(cls, filename):
-        print(f'Loading image classifier from {filename}')
+        print(f'正在從 {filename} 載入圖像分類器')
         return utils.torch_load(filename)
